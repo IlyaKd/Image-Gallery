eef31e5107fa86c5c11f1319cdf4bfe83e4ba727
IlyaKd <ilyakd@protonmail.com>
2021-06-18 14:28:55 +0300

    modified active slide

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
+	const slides = document.querySelectorAll('.slide');
+	slides[activeSlide].classList.add('active');
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClases();
-        slide.classList.add('active');
-    })
+	for (const slide of slides) {
+	    slide.addEventListener('click', () => {
+	        clearActiveClases();
+	        slide.classList.add('active');
+	    })
+	}
+
+	function clearActiveClases() {
+	    slides.forEach(slide => {
+	        slide.classList.remove('active');
+	    })
+	}
 }
 
-function clearActiveClases() {
-    slides.forEach(slide => {
-        slide.classList.remove('active');
-    })
-}
+slidesPlugin();
